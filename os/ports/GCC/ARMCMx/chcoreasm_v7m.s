4c46639b7c750f9eb35da8590020e59777b1a530
Brent Roman <brent@mbari.org>
2020-04-03 11:41:38 -0700

    Initialize PSP before first call in case Resetting with PSP active

--- os/ports/GCC/ARMCMx/chcoreasm_v7m.s
+++ os/ports/GCC/ARMCMx/chcoreasm_v7m.s
@@ -136,12 +136,12 @@ ResetHandler:
          movw    r0, #:lower16:__process_stack_end__
          movt    r0, #:upper16:__process_stack_end__
          msr     PSP, r0
-#if CRT0_INIT_STACKS
-         bl _init_process_stack
-#endif
          mov     r1, #CRT0_CONTROL_INIT
          msr     CONTROL, r1
          isb
          /* Running on process stack.*/
+#if CRT0_INIT_STACKS
+         bl _init_process_stack
+#endif
          b _start
 
